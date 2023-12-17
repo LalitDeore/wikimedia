fdaa9c364bf057d22a05e7025d119b7772353066
lalit <lalitdeore12@gmail.com>
2023-12-18 03:59:42 +0530

    cors updated

--- backend/index.js
+++ backend/index.js
@@ -229,11 +229,17 @@ app.get("/most-searched-keywords/:order(asc|desc)", async (req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://wikimedia-raxl.vercel.app"
+  );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://wikimedia-raxl.vercel.app"
+  );
 
   next();
 });
